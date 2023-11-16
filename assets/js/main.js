e5e046fb5c4ab2296c276f434d19db9fc000b1e2
bramcyd <bramcahyadi05@gmail.com>
2023-11-16 09:20:16 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,6 +178,67 @@
   });
 
   /**
+   * Galery isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let galeryContainer = select('.galery-container');
+    if (galeryContainer) {
+      let galeryIsotope = new Isotope(galeryContainer, {
+        itemSelector: '.galery-item',
+        layoutMode: 'fitRows'
+      });
+
+      let galeryFilters = select('#galery-flters li', true);
+
+      on('click', '#galery-flters li', function(e) {
+        e.preventDefault();
+        galeryFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        galeryIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate galery lightbox 
+   */
+  const galeryLightbox = GLightbox({
+    selector: '.galery-lightbox'
+  });
+
+  /**
+   * Initiate galery details lightbox 
+   */
+  const galeryDetailsLightbox = GLightbox({
+    selector: '.galery-details-lightbox',
+    width: '90%',
+    height: '90vh'
+  });
+
+  /**
+   * Galery details slider
+   */
+  new Swiper('.galery-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
